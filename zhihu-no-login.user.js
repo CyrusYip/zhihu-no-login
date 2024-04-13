d959b4a503759e20ca3fdd2fe82d937934ff4731
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2024-04-13 17:58:56 +0800

    increment version

--- zhihu-no-login.user.js
+++ zhihu-no-login.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         zhihu-no-login
 // @namespace    https://github.com/CyrusYip/zhihu-no-login
-// @version      1.0
+// @version      1.1
 // @description  Redirect login page of zhihu.com to explore page so that you can browse Zhihu without logging in. 将知乎登陆页面跳转到发现页面，这样不用登陆也能看知乎。
 // @author       Cyrus Yip
 // @grant        none
